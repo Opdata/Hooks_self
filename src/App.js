56d1756cdc911b0b177b12f29f8406fa42771e11
Lee Sung Jun <34171875+Opdata@users.noreply.github.com>
2020-02-04 16:44:11 +0900

    useEffect 테스트

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import React, { useState } from "react";
-
-const content = [
-  {
-    name: "section1",
-    content: "The text of the section1"
-  },
-  {
-    name: "section2",
-    content: "The text of the section2"
-  }
-];
-
-const useInput = (init, tabs) => {
-  const [index, setindex] = useState(init);
-  return {
-    contentText: tabs[index],
-    changeitem: setindex
-  };
-};
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const { contentText, changeitem } = useInput(0, content);
+  const Hello = () => {
+    console.log("hello");
+  };
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  useEffect(Hello);
+  /*
+    useEffect(callback, deps)
+    본래 기능은 componentDidmount, componentWillUnmount, componentDidupdate 이다.
+    dpes 에 리스트가 들어가면 그 리스트의 값이 변화가 있을때만 useEffect 실행된다.
+    ex) useEffect(Hello, [number1]) // number1 의 값 변화가 있을때만 실행된다.
+
+    그냥 실행방지는
+    useEffect(Hello, [])
+  */
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button
-          onClick={() => {
-            changeitem(index);
-          }}
-        >
-          {section.name}
-        </button>
-      ))}
-      <div>{contentText.content}</div>
+      <div>hi</div>
+      <button onClick={() => setNumber1(number1 + 1)}>{number1}</button>
+      <button onClick={() => setNumber2(number2 + 1)}>{number2}</button>
     </div>
   );
 };
