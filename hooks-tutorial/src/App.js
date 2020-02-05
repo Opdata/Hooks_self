f0e93b412eee384296a1c51fd35d45cc87d9de5d
Opdata <pb03641@gmail.com>
2020-02-05 15:37:40 +0900

    useState cleanup 테스트

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 // import Counter from "./Counter";
 import Info from "./info";
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
     //<Counter />;
-    <Info />
+    <div>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "보이기" : "숨기기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
   );
 };
 
--- hooks-tutorial/src/info.js
+++ hooks-tutorial/src/info.js
@@ -3,9 +3,32 @@ import React, { useState, useEffect } from "react";
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+  /*
+  useEffect(() => {
+    console.log('마운트될 때만 실행된다.)
+  }, []);
+  */
+
+  /*
+  useEffect(() => {
+    console.log('name 상태가 변경될때만 실행된다.)
+  }, [name]);
+  */
+
+  /*
   useEffect(() => {
     console.log("렌더링 완료");
     console.log({ name, nickname });
+  });
+  */
+
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
   });
   const onChangeName = e => {
     setName(e.target.value);
