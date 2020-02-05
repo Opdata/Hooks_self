0cf6db6e29ea68ad91618663e8895ee06bd19fba
Lee Sung Jun <34171875+Opdata@users.noreply.github.com>
2020-02-05 10:47:52 +0900

    useEffect 테스트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
 
-const App = () => {
-  const Hello = () => {
-    console.log("hello");
+const UpdateTitle = init => {
+  const [title, setTitle] = useState(init);
+  const Update = () => {
+    const htmltitle = document.querySelector("title");
+    htmltitle.innerText = title;
   };
-  const [number1, setNumber1] = useState(0);
-  const [number2, setNumber2] = useState(0);
-  useEffect(Hello);
-  /*
-    useEffect(callback, deps)
-    본래 기능은 componentDidmount, componentWillUnmount, componentDidupdate 이다.
-    dpes 에 리스트가 들어가면 그 리스트의 값이 변화가 있을때만 useEffect 실행된다.
-    ex) useEffect(Hello, [number1]) // number1 의 값 변화가 있을때만 실행된다.
+  useEffect(Update, [title]);
+  return setTitle;
+};
 
-    그냥 실행방지는
-    useEffect(Hello, [])
-  */
+const App = () => {
+  const title = UpdateTitle("Loading");
+  console.log(title);
+  setTimeout(() => title("home"), 5000);
   return (
     <div className="App">
-      <div>hi</div>
-      <button onClick={() => setNumber1(number1 + 1)}>{number1}</button>
-      <button onClick={() => setNumber2(number2 + 1)}>{number2}</button>
+      <h1>hi</h1>
     </div>
   );
 };
